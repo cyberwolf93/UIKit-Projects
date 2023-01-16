130b89bc21eba1a7dba6d24ed1c7727e76fc1128
Ahmed Mohiy <amohiy@amohiy.local>
2023-01-16 21:34:45 +0200

    Create application main files

---
+++ LinkedIn Replica/linkedIn Replica/linkedIn ReplicaUITests/linkedIn_ReplicaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  linkedIn_ReplicaUITestsLaunchTests.swift
+//  linkedIn ReplicaUITests
+//
+//  Created by Ahmed Mohiy on 16/01/2023.
+//
+
+import XCTest
+
+final class linkedIn_ReplicaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
