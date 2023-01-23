64491599660f1bcb7f28692f9ea23a3e276d843a
Ahmed Mohiy <amohiy@amohiy.local>
2023-01-23 19:20:55 +0200

    create models modul

---
+++ LinkedIn Replica/linkedin_models/linkedin_models/linkedin_models.h
@@ -0,0 +1,18 @@
+//
+//  linkedin_models.h
+//  linkedin_models
+//
+//  Created by Ahmed Mohiy on 23/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for linkedin_models.
+FOUNDATION_EXPORT double linkedin_modelsVersionNumber;
+
+//! Project version string for linkedin_models.
+FOUNDATION_EXPORT const unsigned char linkedin_modelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <linkedin_models/PublicHeader.h>
+
+
